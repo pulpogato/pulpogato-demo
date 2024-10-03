dd6f61eb7517e8f68be3daa2f7efc343752b15d8
Rahul Somasunderam <rahulsom@noreply.github.com>
2024-10-03 19:38:49 +0530

    Initial commit

---
+++ src/main/java/io/github/pulpogato/demo/PulpogatoDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.pulpogato.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulpogatoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulpogatoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pulpogato/demo/PulpogatoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pulpogato.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulpogatoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
