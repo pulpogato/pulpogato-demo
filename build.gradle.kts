43d8a9a54daf7d3e7a4ae058f2d8fe6240561ca4
Rahul Somasunderam <rahul.som@gmail.com>
2024-10-04 14:28:44 +0530

    chore: Add .editorconfig
    And reformat code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,38 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
+    java
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "io.github.pulpogato"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 extra["netflixDgsVersion"] = "9.1.2"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:${property("netflixDgsVersion")}")
-	}
+    imports {
+        mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:${property("netflixDgsVersion")}")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
